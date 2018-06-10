5e9621d6b647f1c27e15b8f7f56b1f2c7d7da5f8
cthwaite <thwaite@mac.com>
2018-06-10 13:34:42 +0100

    Update cogs example

--- examples/cogs.rs
+++ examples/cogs.rs
@@ -5,13 +5,12 @@ use annals::{Cognate, Context, Scribe};
 use std::fs::File;
 
 fn main() {
-    let f = File::open("cogs.yml").unwrap();
-    let cogs : Vec<Cognate> = serde_yaml::from_reader(f).unwrap();
     let mut scribe = Scribe::new();
-    cogs.into_iter().for_each(|cog| scribe.insert_cognate(cog));
+    scribe.load_cognates("texts/cogs.yml").unwrap();
     for _i in 0..5 {
-        println!("{}", scribe.gen("<greeting>").unwrap());
+        println!("{}", scribe.gen("greeting").unwrap());
     }
+    println!("{}", scribe.gen("root").unwrap());
     let mut ctx = Context::new();
     ctx.set("colour", "red");
     println!("{}", scribe.gen_with("greeting", ctx).unwrap());
