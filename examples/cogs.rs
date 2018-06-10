4fc7913616f6319a45f246bb667a9089f17dc04a
cthwaite <thwaite@mac.com>
2018-06-10 13:35:56 +0100

    Update cogs example to use root/bindings template

--- examples/cogs.rs
+++ examples/cogs.rs
@@ -8,10 +8,10 @@ fn main() {
     let mut scribe = Scribe::new();
     scribe.load_cognates("texts/cogs.yml").unwrap();
     for _i in 0..5 {
-        println!("{}", scribe.gen("greeting").unwrap());
+        println!("{}", scribe.gen("root").unwrap());
     }
     println!("{}", scribe.gen("root").unwrap());
     let mut ctx = Context::new();
     ctx.set("colour", "red");
-    println!("{}", scribe.gen_with("greeting", ctx).unwrap());
+    println!("{}", scribe.gen_with("root", ctx).unwrap());
 }
