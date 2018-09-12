2ca58208f83e923b29051c0344c5dcc19d51fcc2
cthwaite <thwaite@mac.com>
2018-09-12 22:02:57 +0100

    Add HMS example ported from improv

---
+++ examples/hms.rs
@@ -0,0 +1,11 @@
+extern crate annals;
+use annals::Scribe;
+
+fn main() {
+    let mut scribe = Scribe::new();
+    scribe.load_cognates("texts/hms.yml").unwrap();
+    for _i in 0..4 {
+        println!("{}", scribe.gen("root").unwrap());
+        println!("");
+    }
+}
