b9e6e38e90e41208a4366838dc71358eb8e110f0
cthwaite <thwaite@mac.com>
2018-09-13 19:25:39 +0100

    Update readme example to be more comprehensive

--- examples/readme.rs
+++ examples/readme.rs
@@ -1,5 +1,5 @@
 extern crate annals;
-use annals::Scribe;
+use annals::{Context, Scribe};
 
 const YAML_STR : &str = r#"
 ---
@@ -16,20 +16,25 @@ const YAML_STR : &str = r#"
 - name: expression
   groups:
   - rules:
-    - "ANY: what a beautiful <animal>"
-    - "ANY: oh, look at that <animal>!"
+    - "<@speaker>: what a beautiful <animal>"
+    - "<@speaker>: oh, look at that <animal>!"
   - tags: { "size": "big" }
     rules:
-    - "BIG: wow! look at the size of that <animal>"
+    - "<@speaker>: wow! look at the size of that <animal>"
+    - "<@speaker>: there's nothing quite like seeing <#1-100> wild <animal>s"
   - tags: { "size": "small" }
     rules:
-    - "SMALL: oh look! a tiny <animal>!"
+    - "<@speaker>: oh look! a tiny <animal>!"
+    - "<@speaker>: i just saw <#1-100> tiny <animal> babies at the zoo"
+    - "<@speaker>: is that <(an !animal)>? yes, it's a tiny <animal>!"
 "#;
 
 fn main() {
     let mut scribe = Scribe::new();
     scribe.load_cognates_str(YAML_STR).unwrap();
+    let mut ctx = Context::default();
+    ctx.bind("speaker", "Bob");
     for _i in 0..10 {
-        println!("{}", scribe.gen("expression").unwrap());
+        println!("{}", scribe.gen_with("expression", ctx.clone()).unwrap());
     }
 }
